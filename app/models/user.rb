f61c3c954a970622fcd9dbc9bda2955ef5b04e03
Yung Dai <yungchidai@gmail.com>
2014-12-01 18:48:29 -0500

    beginning to add sorcery, had to change the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  authenticates_with_sorcery!
+
   has_many :owned_projects, class_name: 'Project', foreign_key: 'owner_id', inverse_of: :owner
   has_many :pledges
   has_many :projects, through: :pledges
--- db/migrate/20141201194349_create_users.rb
+++ db/migrate/20141201194349_create_users.rb
@@ -3,10 +3,13 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
-      t.string :password_digest
+      t.string :email,            :null => false
+      t.string :crypted_password, :null => false
+      t.string :salt,             :null => false
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
