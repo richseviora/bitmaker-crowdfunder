a0b47942da95b6d3522448a933f1922b2106ba88
Rich Seviora <richard.seviora@gmail.com>
2014-12-01 16:35:19 -0500

    Fixing User > Projects relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :owned_projects, class_name: 'Project', foreign_key: 'owner_id', inverse_of: :owner
   has_many :pledges
-  has_many :backed_projects, through: :pledges, class_name: 'Project'
+  has_many :projects, through: :pledges
 end
