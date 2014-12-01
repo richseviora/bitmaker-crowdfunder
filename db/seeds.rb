39d377da8ee92465eaca829beca596dde3598a06
Rich Seviora <richard.seviora@gmail.com>
2014-12-01 16:00:55 -0500

    Adding Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+u = User.create(first_name: 'Rich', last_name: 'Seviora', email: 'richard.seviora@gmail.com', password: 'bob')
+
+p = Project.new
+p.name = 'Bob'
+p.description = 'Desc'
+p.start_date = Date.new(2014,12,12)
+p.end_date = Date.new(2014,12,31)
+p.funding_goal = 50000
+p.owner = u
+p.save
+
+reward = Reward.create(title: 'First Reward', description: 'description here', amount: 10, project: Project.first)
+pledge = Pledge.create(amount: 10, reward: Reward.first, user: User.first)
