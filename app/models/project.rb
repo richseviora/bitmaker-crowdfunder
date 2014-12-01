b4bfefd75e23d845969aa29b8f2624243b671df6
Rich Seviora <richard.seviora@gmail.com>
2014-12-01 15:59:17 -0500

    Tweaking User/Project Associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
   has_many :rewards
-  belongs_to :owner, class_name: 'User'
+  belongs_to :owner, class_name: 'User', inverse_of: :owned_projects
   has_many :pledges, through: :rewards
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :owned_projects, class_name: 'Project'
+  has_secure_password
+  has_many :owned_projects, class_name: 'Project', foreign_key: 'owner_id', inverse_of: :owner
   has_many :pledges
   has_many :backed_projects, through: :pledges, class_name: 'Project'
 end
