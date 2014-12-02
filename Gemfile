db57139e209719caedd88f1c556aff1c1677df06
Yung Dai <yungchidai@gmail.com>
2014-12-02 10:36:48 -0500

    cleaned up the Gemfile to make sure that sorcery is is only using 0.8.3 or higher, and that cleaned up the user migration file to ensure that first_name and last_name can’t be null and made sure that the email field is unique.

--- Gemfile
+++ Gemfile
@@ -30,7 +30,7 @@ gem 'spring',        group: :development
 # gem 'bcrypt', '~> 3.1.7'
 
 # User Sorcery for Authentication
-gem 'sorcery'
+gem 'sorcery', '0.8.6'
 
 # Use unicorn as the app server
 # gem 'unicorn'
--- db/migrate/20141201194349_create_users.rb
+++ db/migrate/20141201194349_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
+      # <:null => false> prevents the field from being empty
+      t.string :first_name,       :null => false
+      t.string :last_name,        :null => false
       t.string :email,            :null => false
       t.string :crypted_password, :null => false
       t.string :salt,             :null => false
@@ -10,6 +11,6 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email, unique: true
+    add_index :email, unique: true
   end
 end
