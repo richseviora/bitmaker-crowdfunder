c674738db8d5f3dbba9c474158eb81a027b255fb
Yung Dai <yungchidai@gmail.com>
2014-12-04 13:16:28 -0500

    Revert "Revert "most recent commit while working with Maria and Andrew""
    This reverts commit d34cb39de6d8587b8c312daf2ed6b993ba876dd0.

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -7,7 +7,7 @@ class PledgesController < ApplicationController
 		# find the reward based on current :reward_id
 		@pledge.amount = Reward.find(params[:reward_id]).amount
 		@pledge.save
-		
+
 		 respond_to do |format|
 		 	format.js  #allows controller to respond in javascript
 		 	format.html { redirect_to project_url(params[:project_id]) } #allows controller to respond to html
