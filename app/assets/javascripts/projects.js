c24ded8d4773dcc386dfe3ae1b20e39b000b895d
chaugirl <chau.marialuisa@gmail.com>
2014-12-04 16:27:35 -0500

    AJAX request now working for iteration 1.

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -3,14 +3,17 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 // "/pledges?project_id=20&reward_id=44"
 
-$(document).ready(function () {
-    $('.rewards-form').submit(function (event) {
-        event.preventDefault();
 
+// when the DOM loads
+$(document).ready(function () {
+    // gets rewards-form (class) from show.html.erb (reward form)
+    $('.rewards-form').submit(function (event) {
+        // prevent the pledeges/create.html.erb from loading after pressing button
+        event.preventDefault();
         // STANDARD - works all the time!
         var myurl = $(this).attr('action');
         $.ajax({
-            url: myurl, //'/pledges?reward_id=' + rewardValue,
+            url: myurl, 
             type: 'POST',
             dataType: 'script'
         }).done(function (data) {
@@ -18,7 +21,6 @@ $(document).ready(function () {
         }).fail(function (error) {
             alert("request failed!");
         });
-    	// $.getScript('/pledges?reward_id=' + rewardValue);
-    	alert('Went through projects.js');
+    	// END OF STANDARD AJAX REQUEST
     });
 });
--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,14 +1,19 @@
 class PledgesController < ApplicationController
 
 	def create
+		# putting @project so @project will be binded to the create method
+		# now you can use @project in the projects/_individual_project.html.erb
+		@project = Project.find(params[:project_id])
 		@pledge = Pledge.new(reward: Reward.find(params[:reward_id]))
 		@pledge.user = current_user
 		@pledge.amount = Reward.find(params[:reward_id]).amount
+		#assigning the pledge to the project
+		@pledge.project = @project
 		@pledge.save
 
 		respond_to do |format|
-      		format.html # create.html.erb
       		format.js   # create.js.erb, allows controller to respond to JavaScript
+      		format.html { redirect_to project_url(params[:project_id]) }# create.html.erb
     	end
 	end
 end
