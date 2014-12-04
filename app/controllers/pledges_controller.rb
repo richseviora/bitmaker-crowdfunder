580fb997b7e496bf89096e67acc22177ffbed80f
Yung Dai <yungchidai@gmail.com>
2014-12-03 21:22:22 -0500

    did some commenting.

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -6,14 +6,16 @@ class PledgesController < ApplicationController
 
 	def create
 		@pledge = Pledge.new(reward: Reward.find(params[:reward_id]))
+		# ensure that the pledge user is the current user
 		@pledge.user = current_user
+		# find the reward based on current :reward_id
 		@pledge.amount = Reward.find(params[:reward_id]).amount
 		@pledge.save
-		# @pledge.amount = reward.amount	
-		# respond_to do |format|
-		# 	format.js { render text: 'Hi there' } #allows controller to respond in javascript
-		# 	format.html #allows controller to respond to html
-		# end
+
+		 respond_to do |format|
+		 	format.js { render text: 'Hi there' } #allows controller to respond in javascript
+		 	format.html #allows controller to respond to html
+		 end
 	end
 
 	
